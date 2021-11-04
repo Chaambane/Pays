0414783f42d467a671b8d6f4033b46f6e7cb0b4e
FAIDINE <mohamed.faidine@live.fr>
2021-11-04 21:40:23 +0100

    first commit

---
+++ src/conteners/PaysManager/PaysManager.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PaysManager extends Component {
+    render() {
+        return (
+            <main className="container">
+                <h1 className=" bg-info mt-2 rounded text-center text-white fw-bold p-2">Pays</h1>
+            </main>
+        )
+    };
+}
+
+export default PaysManager;
