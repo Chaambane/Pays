7264eb2acd7a6484ca987cd03b30c594c0d51b49
FAIDINE <mohamed.faidine@live.fr>
2021-11-08 09:44:01 +0100

    refact(TitleH1) add componant name

--- src/components/TitleH1/TitleH1.js
+++ src/components/TitleH1/TitleH1.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-    const component = (props) => (
+    const titleH1 = (props) => (
         <h1 className=" bg-info mt-2 rounded text-center text-white fw-bold p-2">
             {props.children}
         </h1>
     );
 
-export default component;
+export default titleH1;
