39f0f7f7007bc20313cc881599f0ce6a128ba4b9
FAIDINE <mohamed.faidine@live.fr>
2021-11-10 11:20:09 +0100

    feat(Navbar & react router) add navbar and reacter router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import PaysManager from "./conteners/PaysManager/PaysManager";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
-    <div>
-      <PaysManager/>
-    </div>
-  );
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<h1 className="fw-bold container text-primary">Accueil</h1>}/>
+        <Route path="/pays"  element={<PaysManager/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const navbar = (props) => (
+    <nav className="container-fluid navbar navbar-expand-lg navbar-info bg-info p-2 mt-1">
+        <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav fw-bold">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/" exact>Accueil</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/pays" exact>Les pays</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
+
+export default navbar;
