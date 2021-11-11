621f43d22059381991d705afb534e95196b6f8c0
FAIDINE <mohamed.faidine@live.fr>
2021-11-11 14:58:25 +0100

    refact(Navbar) correction of the display button toggle

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const navbar = (props) => (
     <nav className="container-fluid navbar navbar-expand-lg navbar-info bg-info p-2 mt-1">
         <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+            <button className="navbar-toggler text-white fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                Menu<span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav fw-bold">
@@ -13,7 +13,7 @@ const navbar = (props) => (
                         <NavLink className="nav-link" to="/" exact>Accueil</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/pays" exact>Les pays</NavLink>
+                        <NavLink className="nav-link" to="/pays">Les pays</NavLink>
                     </li>
                 </ul>
             </div>
